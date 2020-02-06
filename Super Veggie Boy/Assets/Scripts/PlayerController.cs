6f1f01b44e8a9331aa6f13d538ce68e93bdde18d
Matthieu Heras <matthieu.heras@isen.yncrea.fr>
2020-02-06 15:35:56 +0100

    Setup HRDF Unity project
    - Setup Unity 2019.3.0f6 HRDF
    - Added simple controller

---
+++ Super Veggie Boy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 100f;
+    [SerializeField] private float mouseSensitivity = 50f;
+
+    private CharacterController controller;
+    private Transform playerTransform;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        controller.Move(speed * Time.deltaTime * Input.GetAxis("Vertical") * playerTransform.forward + speed * Time.deltaTime * Input.GetAxis("Horizontal") * playerTransform.right);
+        playerTransform.Rotate(Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime * Vector3.up);
+    }
+}
